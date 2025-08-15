d5477d422a529a5c6e94a0da8cbfe4329da7b2a4
Florian Bauer <fpb255@gmx.de>
2025-08-16 00:47:48 +0200

    simple main application

--- src/main/java/org/florianbauer/lichessbot/Main.java
+++ src/main/java/org/florianbauer/lichessbot/Main.java
@@ -1,8 +1,19 @@
 package org.florianbauer.lichessbot;
 
+import java.io.IOException;
+import org.florianbauer.lichessbot.controller.ChessGameController;
+
 public class Main {
 
   public static void main(String[] args) {
     System.out.println("Hello world!");
+    String token = System.getenv("LICHESS_TOKEN");
+    try {
+      ChessGameController controller = new ChessGameController(token);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
   }
 }
